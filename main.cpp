da8b66af2a034d0eb8754af3c555ab5bb2950d4a
Adrian <adrian.kula24@gmail.com>
2023-02-28 19:32:49 +0100

    feature: Added custom parameter values

--- main.cpp
+++ main.cpp
@@ -14,7 +14,24 @@ int main(int args, char *argv[]){
     std::string RESULT_FILE_NAME = std::filesystem::path(argv[0]).filename().string()+".txt";
     long NUMBER_OF_THREADS = 4;
 
-
+    if(args>2){
+        for(int i=2; i<args; i+=2){
+            if((std::string)argv[i]=="-d" || (std::string)argv[i]=="--dir" ){
+                START_DIRECTORY = (std::string) argv[i+1];
+            }
+            if((std::string)argv[i]== "-l" || (std::string)argv[i]=="--log_file" ){
+                LOG_FILE_NAME = (std::string) argv[i+1]+".log";
+            }
+            if((std::string)(argv[i])=="-r" || (std::string)argv[i]=="--result_file" ){
+                RESULT_FILE_NAME = (std::string) argv[i+1]+".txt";
+            }
+            if((std::string)(argv[i])=="-t" || (std::string)argv[i]=="--threads" ){
+                char* p;
+                long arg = strtol(argv[i+1], &p, 10);
+                NUMBER_OF_THREADS = arg;
+            }
+        }
+    }
     std::cout << START_DIRECTORY << std::endl;
     std::cout << LOG_FILE_NAME << std::endl;
     std::cout << RESULT_FILE_NAME << std::endl;
