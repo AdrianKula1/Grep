d703c481b41f0ee92e45f0ca352d6b7f2f5d6c76
Adrian <adrian.kula24@gmail.com>
2023-03-13 18:43:45 +0100

    Feature: Added usage of unique_ptr in main and slightly refactored the code

--- main.cpp
+++ main.cpp
@@ -1,36 +1,37 @@
 #include <iostream>
+#include <memory>
 
 #include "Grep.h"
 
 namespace fs=std::filesystem;
 
-void displayResults(Grep *g, long long int elapsedTime){
-    std::cout << "Searched files: " << g->getSearchedFiles() << std::endl;
-    std::cout << "Files with pattern: " << g->getFilesWithPattern() << std::endl;
-    std::cout << "Patterns number: " << g->getPatternsNumber() << std::endl;
-    std::cout << "Result file: " << g->getResultFileName() << std::endl;
-    std::cout << "Log file: " << g->getLogFileName() << std::endl;
-    std::cout << "Used threads: " << g->getNumberOfThreads() << std::endl;
+void displayResults(std::unique_ptr<Grep> &grep, long long int elapsedTime){
+    std::cout << "Searched files: " << grep->getSearchedFiles() << std::endl;
+    std::cout << "Files with pattern: " << grep->getFilesWithPattern() << std::endl;
+    std::cout << "Patterns number: " << grep->getPatternsNumber() << std::endl;
+    std::cout << "Result file: " << grep->getResultFileName() << std::endl;
+    std::cout << "Log file: " << grep->getLogFileName() << std::endl;
+    std::cout << "Used threads: " << grep->getNumberOfThreads() << std::endl;
     std::cout << "Elapsed time: " << elapsedTime  << "[ms]"<< std::endl;
 }
 
 int main(int argc, char *argv[]){
     std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-    Grep *g;
+    std::unique_ptr<Grep> grep;
 
     try{
-        g=new Grep(argc, argv);
+        grep = std::make_unique<Grep>(argc, argv);
     }catch (std::exception &e){
         std::cout << e.what() << std::endl;
         return 0;
     };
 
-    g->main();
+    grep->main();
 
     std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
     long long int elapsedTime = std::chrono::duration_cast<std::chrono::milliseconds>(end-begin).count();
 
-    displayResults(g, elapsedTime);
+    displayResults(grep, elapsedTime);
 
     return 0;
 }
